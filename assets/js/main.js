ef876ba68a52c61320b5ec80983f7001edde0420
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-01 15:15:06 +0100

    testing

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+console.log("test");
+/* 
+
+function 
+    1. alles aus array mit * 2
+    2. sortieren
+
+*/
+
+let array = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+  ];
+  
+
+
+  function neuesArray(){
+ array.sort((a,b) => a-b);
+console.table(array);
+
+array.map((element) => {
+    const body = document.querySelector("body");
+    body.innerHTML += ` ${element*2} </br> `
+
+})
+
+console.table(array);
+  }
+
+  neuesArray();
+  
